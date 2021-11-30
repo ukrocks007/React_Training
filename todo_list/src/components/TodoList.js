a982e7a7b0e477f9e2494a33d90c54d8e2fba827
Utkarsh Mehta <utkarsh@tarkalabs.com>
2021-11-30 16:20:37 +0530

    final changes

--- todo_list/src/components/TodoList.js
+++ todo_list/src/components/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import CreateTodo from './createTodo';
-import {Card, Stack, ListGroupItem, ListGroup, Badge, Row, Col, Button} from 'react-bootstrap';
+import {Card, Stack, ListGroupItem, ListGroup, Badge, Row, Col, Button, Form} from 'react-bootstrap';
 
 export default class TodoList extends React.Component {
     constructor(props) {
@@ -9,7 +9,9 @@ export default class TodoList extends React.Component {
         this.state = {
             todos: [],
             name: "",
-            tasks: []
+            tasks: [],
+            isUpdate: false,
+            todoId: undefined
         }
         this.getLists = this.getLists.bind(this);
         this.logout = this.logout.bind(this);
@@ -28,9 +30,24 @@ export default class TodoList extends React.Component {
             .catch(err => console.log(err));
     }
 
+    edit(id) {
+        let todo = this.state.todos.filter(a => a.id == id)[0];
+        this.setState(
+            {
+                eName: todo.name,
+                isUpdate: true,
+                todoId: id
+            }
+        )
+    }
+
     logout() {
         localStorage.clear();
         window.location.replace('/');
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
     }
 
     getLists() {
@@ -51,16 +68,25 @@ export default class TodoList extends React.Component {
                 <Row>
                     <Col md={{ span: 3, offset: 10 }} ><Button onClick={this.logout}>Logout</Button></Col>
                 </Row>
-                <CreateTodo refreshLists={this.getLists} />
-                <Stack direction="horizontal" gap={3}>
-                { this.state.todos.map(t =>
-                    <Card style={ { width: '18rem' } }>
-                        <Card.Body>
-                            <Card.Title>{ t.name }</Card.Title>
-                            <ListGroup className="list-group-flush">
+                <CreateTodo refreshLists={ this.getLists } />
+                <Stack direction="horizontal" gap={ 3 }>
+                    { this.state.todos.map(t =>
+                        <Card key={t.id} style={ { width: '18rem' } }>
+                            <Card.Body>
                                 {
-                                    t.list.map(tsk =>
-                                        <ListGroupItem>{ tsk.data }  { tsk.isDone && <Badge pill bg="success">
+                                    this.state.isUpdate ?
+                                        <Form.Group className="mb-3" controlId="createTodoList">
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control type="text" name="eName" placeholder="Enter Todo List Name"
+                                                value={ this.state.eName } onChange={ this.handleChange } />
+                                        </Form.Group>
+                                        :
+                                        <Card.Title>{ t.name }</Card.Title>
+                                }
+                                <ListGroup className="list-group-flush">
+                                {
+                                    t.list.map((tsk, i) =>
+                                        <ListGroupItem key={'tsk' + i}>{ tsk.data }  { tsk.isDone && <Badge pill bg="success">
                                             Done
                                         </Badge> }
                                         </ListGroupItem>
--- todo_list/src/components/createTodo.js
+++ todo_list/src/components/createTodo.js
@@ -13,34 +13,63 @@ const colors = [
     'Light',
 ];
 
+var oneTimeFlag = false;
 export default class CreateTodo extends React.Component {
     constructor(props) {
         super(props);
+        debugger;
         this.state = {
-            name: "",
-            tasks: [],
-            newTask: ""
+            name: (props.isUpdate && props.todoItem) ? props.todoItem.name : "",
+            tasks: (props.isUpdate && props.todoItem) ? props.todoItem.list : [],
+            newTask: "",
+            isUpdate: props.isUpdate || false,
+            item: props.todoItem || undefined,
+            oneTimeFlag: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.getTasksList = this.getTasksList.bind(this);
         this.addTodo = this.addTodo.bind(this);
     }
 
+    componentDidMount() {
+        this.setState({
+            name: (this.props.isUpdate && this.props.todoItem) ? this.props.todoItem.name : "",
+            tasks: (this.props.isUpdate && this.props.todoItem) ? this.props.todoItem.list : [],
+            newTask: "",
+            isUpdate: this.props.isUpdate || false,
+            item: this.props.todoItem || undefined
+        });
+    }
+
     addTodo(e) {
         let user = localStorage.getItem('user');
         user = user ? JSON.parse(user) : undefined;
         if (user) {
-            axios.post('https://63af-2409-4042-4d32-6d94-a5e4-2ceb-ff06-6b44.ngrok.io/api/todo', {
-                name: this.state.name,
-                list: this.state.tasks,
-                user: user.id
-            }, {
-                headers: {
-                    "Authorization": localStorage.getItem('token')
-                }
-            })
-                .then(res => { console.log(res.data); this.props.refreshLists(); this.setState({tasks: []}); })
-                .catch(err => console.log)
+            if (this.state.isUpdate) {
+                axios.put('https://63af-2409-4042-4d32-6d94-a5e4-2ceb-ff06-6b44.ngrok.io/api/todo', {
+                    name: this.state.name,
+                    list: this.state.tasks,
+                    user: user.id
+                }, {
+                    headers: {
+                        "Authorization": localStorage.getItem('token')
+                    }
+                })
+                    .then(res => { console.log(res.data); this.props.refreshLists(); this.setState({ tasks: [] }); })
+                    .catch(err => console.log)
+            } else {
+                axios.post('https://63af-2409-4042-4d32-6d94-a5e4-2ceb-ff06-6b44.ngrok.io/api/todo', {
+                    name: this.state.name,
+                    list: this.state.tasks,
+                    user: user.id
+                }, {
+                    headers: {
+                        "Authorization": localStorage.getItem('token')
+                    }
+                })
+                    .then(res => { console.log(res.data); this.props.refreshLists(); this.setState({ tasks: [] }); })
+                    .catch(err => console.log)
+            }
         }
     }
 
@@ -53,42 +82,53 @@ export default class CreateTodo extends React.Component {
     }
 
     render() {
+        if(this.props.isUpdate && !oneTimeFlag) {
+            debugger;
+            this.state = {
+                name: (this.props.isUpdate && this.props.todoItem) ? this.props.todoItem.name : "",
+                tasks: (this.props.isUpdate && this.props.todoItem) ? this.props.todoItem.list : [],
+                newTask: "",
+                isUpdate: this.props.isUpdate || false,
+                item: this.props.todoItem || undefined,
+            };
+            oneTimeFlag=true;
+        }
         return (
-            <div>
-                <Form>
-                    <Form.Group className="mb-3" controlId="createTodoList">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" name="name" placeholder="Enter Todo List Name"
-                            value={ this.state.name } onChange={ this.handleChange } />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Task Name</Form.Label>
-                        <InputGroup className="mb-3">
-                            <FormControl
-                                placeholder="New Task Name"
-                                aria-label="New Task Name"
-                                aria-describedby="basic-addon2"
-                                name="newTask"
-                                value={ this.state.newTask } onChange={ this.handleChange }
-                            />
-                            <Button variant="outline-primary" onClick={
-                                e =>
-                                    this.setState({
-                                        tasks: [...this.state.tasks, { data: this.state.newTask, isDone: false }],
-                                        newTask: ""
-                                    })
-                            }
+                <div>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="createTodoList">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" name="name" placeholder="Enter Todo List Name"
+                                value={ this.state.name } onChange={ this.handleChange } />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Task Name</Form.Label>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    placeholder="New Task Name"
+                                    aria-label="New Task Name"
+                                    aria-describedby="basic-addon2"
+                                    name="newTask"
+                                    value={ this.state.newTask } onChange={ this.handleChange }
+                                />
+                                <Button variant="outline-primary" onClick={
+                                    e =>
+                                        this.setState({
+                                            tasks: [...this.state.tasks, { data: this.state.newTask, isDone: false }],
+                                            newTask: ""
+                                        })
+                                }
 
-                            >Add New Task</Button>{ ' ' }
-                        </InputGroup>
-                    </Form.Group>
-                    <Button onClick={ this.addTodo }>Add List</Button>
-                </Form>
-                <br />
-                {
-                    this.state.tasks.map((t, i) => <Card bg={ colors[i % colors.length].toLowerCase() } body>{ t.data }</Card>)
-                }
-            </div>
+                                >Add New Task</Button>{ ' ' }
+                            </InputGroup>
+                        </Form.Group>
+                        <Button onClick={ this.addTodo }>{this.state.isUpdate ? "Save" : "Add List"}</Button>
+                    </Form>
+                    <br />
+                    {
+                        this.state.tasks.map((t, i) => <Card bg={ colors[i % colors.length].toLowerCase() } body>{ t.data }</Card>)
+                    }
+                </div>
         );
     }
 }
